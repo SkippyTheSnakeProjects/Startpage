9b08083d1028ae4adb61d4574dfd1c2cb9719c4c
Reid Mason <maddogshain132@gmail.com>
2020-07-19 18:01:00 +0100

    Moved appdata directory

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker stop "Startpage"
 docker rm "Startpage"
-docker run -d --restart unless-stopped --name="Startpage" -p 5003:5003 -e TZ="Europe/London" -v '/home/skippythesnake/appdata/startpage':'/app/data':'rw' 'skippythesnake/startpage'
+docker run -d --restart unless-stopped --name="Startpage" -p 5003:5003 -e TZ="Europe/London" -v '/mnt/appdata/startpage':'/app/data':'rw' 'skippythesnake/startpage'
