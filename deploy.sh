299e0fb8d123664a07b50b6fa314c4709caa7063
Reid Mason <maddogshain123@gmail.com>
2020-11-01 02:16:53 +0000

    Removed build scripts

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker stop "Startpage"
-docker rm "Startpage"
-docker run -d --restart unless-stopped --name="Startpage" -p 5003:5003 -e TZ="Europe/London" -v '/mnt/appdata/startpage':'/app/data':'rw' 'skippythesnake/startpage'
