78540cb06402573125818626315b7d69f5903af3
Reid Mason <maddogshain123@gmail.com>
2021-03-12 23:38:49 +0000

    Put app grid into a section

--- client/src/components/AppsGrid.tsx
+++ client/src/components/AppsGrid.tsx
@@ -6,10 +6,10 @@ export default function AppsGrid() {
     const { config } = useContext(GlobalContext)!;
 
     return (
-        <div className="grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 transition-opacity">
+        <section className="grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 transition-opacity">
             {config && config.apps.map((app) => (
                 <App app={app} key={app.name + app.icon + app.url} />
             ))}
-        </div>
+        </section>
     )
 }
